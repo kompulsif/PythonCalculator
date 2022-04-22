c679a860ad4d86a9171dca68a69b38910ab9e543
Kompulsif <kompulsiff1e@gmail.com>
2022-04-22 12:30:18 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -32,7 +32,6 @@ def keyboard_control(k):
         return "break"
 
 
-
 def writer(b):
 
     cur_i = operation.index(tk.INSERT)
@@ -46,11 +45,9 @@ def writer(b):
             operation.icursor(operation.index(tk.INSERT)-1)
 
 
-
 def deleter():
     i = operation.index(tk.INSERT)
     operation.delete(first=i-1, last=i)
-
 
 
 def calc_func():
@@ -61,7 +58,7 @@ def calc_func():
 
         if ("**" in i):
             raise SyntaxError
-        
+
         i = i.replace("^", "**")
         i = i.replace("π", "3.1415926535897932384626433832795")
         sq_count = i.count("√")
@@ -76,7 +73,7 @@ def calc_func():
 
         i = i.replace("√", "__import__('math').sqrt").strip()
 
-        result = eval(i.replace(" ",""))
+        result = eval(i.replace(" ", ""))
 
     except:
         pass
@@ -86,32 +83,34 @@ def calc_func():
         operation.insert(0, result)
 
 
-
 def clear_func():
     operation.delete(0, "end")
-
 
 
 def advanced_options_func():
     global adOp, pi, us, kr, kl
 
     if (adOp):
-        
-        pi = tk.Button(form, text="π", command=lambda: writer(pi), font="Arial 10 bold")
+
+        pi = tk.Button(form, text="π", command=lambda: writer(
+            pi), font="Arial 10 bold")
         pi.place(relx=0.12, rely=0.88, relwidth=0.16, relheight=0.10)
 
-        us = tk.Button(form, text="^", command=lambda: writer(us), font="Verdana 10 bold")
+        us = tk.Button(form, text="^", command=lambda: writer(
+            us), font="Verdana 10 bold")
         us.place(relx=0.32, rely=0.88, relwidth=0.16, relheight=0.10)
 
-        kl = tk.Button(form, text="%", command=lambda: writer(kl), font="Verdana 10 bold")
+        kl = tk.Button(form, text="%", command=lambda: writer(
+            kl), font="Verdana 10 bold")
         kl.place(relx=0.52, rely=0.88, relwidth=0.16, relheight=0.10)
 
-        kr = tk.Button(form, text="√()", command=lambda: writer(kr), font="Verdana 10 bold")
+        kr = tk.Button(form, text="√()", command=lambda: writer(
+            kr), font="Verdana 10 bold")
         kr.place(relx=0.72, rely=0.88, relwidth=0.16, relheight=0.10)
 
         adop_but.config(text="Hide Advanced Options")
         adOp = 0
-    
+
     else:
 
         pi.destroy()
@@ -122,37 +121,57 @@ def advanced_options_func():
         adOp = 1
 
 
+to = tk.Button(form, text="+", command=lambda: writer(to),
+               font="Verdana 10 bold", bg="white")
+ci = tk.Button(form, text="-", command=lambda: writer(ci),
+               font="Verdana 10 bold", bg="white")
+ca = tk.Button(form, text="*", command=lambda: writer(ca),
+               font="Verdana 10 bold", bg="white")
+bo = tk.Button(form, text="/", command=lambda: writer(bo),
+               font="Verdana 10 bold", bg="white")
 
-to = tk.Button(form, text="+", command=lambda: writer(to), font="Verdana 10 bold", bg="white")
-ci = tk.Button(form, text="-", command=lambda: writer(ci), font="Verdana 10 bold", bg="white")
-ca = tk.Button(form, text="*", command=lambda: writer(ca), font="Verdana 10 bold", bg="white")
-bo = tk.Button(form, text="/", command=lambda: writer(bo), font="Verdana 10 bold", bg="white")
+n1 = tk.Button(form, text="1", command=lambda: writer(n1),
+               font="Verdana 10 bold", bg="white")
+n2 = tk.Button(form, text="2", command=lambda: writer(n2),
+               font="Verdana 10 bold", bg="white")
+n3 = tk.Button(form, text="3", command=lambda: writer(n3),
+               font="Verdana 10 bold", bg="white")
 
-n1 = tk.Button(form, text="1", command=lambda: writer(n1), font="Verdana 10 bold", bg="white")
-n2 = tk.Button(form, text="2", command=lambda: writer(n2), font="Verdana 10 bold", bg="white")
-n3 = tk.Button(form, text="3", command=lambda: writer(n3), font="Verdana 10 bold", bg="white")
+n4 = tk.Button(form, text="4", command=lambda: writer(n4),
+               font="Verdana 10 bold", bg="white")
+n5 = tk.Button(form, text="5", command=lambda: writer(n5),
+               font="Verdana 10 bold", bg="white")
+n6 = tk.Button(form, text="6", command=lambda: writer(n6),
+               font="Verdana 10 bold", bg="white")
 
-n4 = tk.Button(form, text="4", command=lambda: writer(n4), font="Verdana 10 bold", bg="white")
-n5 = tk.Button(form, text="5", command=lambda: writer(n5), font="Verdana 10 bold", bg="white")
-n6 = tk.Button(form, text="6", command=lambda: writer(n6), font="Verdana 10 bold", bg="white")
+n7 = tk.Button(form, text="7", command=lambda: writer(n7),
+               font="Verdana 10 bold", bg="white")
+n8 = tk.Button(form, text="8", command=lambda: writer(n8),
+               font="Verdana 10 bold", bg="white")
+n9 = tk.Button(form, text="9", command=lambda: writer(n9),
+               font="Verdana 10 bold", bg="white")
 
-n7 = tk.Button(form, text="7", command=lambda: writer(n7), font="Verdana 10 bold", bg="white")
-n8 = tk.Button(form, text="8", command=lambda: writer(n8), font="Verdana 10 bold", bg="white")
-n9 = tk.Button(form, text="9", command=lambda: writer(n9), font="Verdana 10 bold", bg="white")
+si = tk.Button(form, text="C", command=clear_func,
+               font="Verdana 10 bold", bg="white")
+n0 = tk.Button(form, text="0", command=lambda: writer(n0),
+               font="Verdana 10 bold", bg="white")
 
-si = tk.Button(form, text="C", command=clear_func, font="Verdana 10 bold", bg="white")
-n0 = tk.Button(form, text="0", command=lambda: writer(n0), font="Verdana 10 bold", bg="white")
+no = tk.Button(form, text=".", command=lambda: writer(no),
+               font="Verdana 10 bold", bg="white")
+de = tk.Button(form, text="Del", command=deleter,
+               font="Verdana 10 bold", bg="white")
 
-no = tk.Button(form, text=".", command=lambda: writer(no), font="Verdana 10 bold", bg="white")
-de = tk.Button(form, text="Del", command=deleter, font="Verdana 10 bold", bg="white")
+p1 = tk.Button(form, text="(", command=lambda: writer(p1),
+               font="Verdana 10 bold", bg="white")
+p2 = tk.Button(form, text=")", command=lambda: writer(p2),
+               font="Verdana 10 bold", bg="white")
 
-p1 = tk.Button(form, text="(", command=lambda: writer(p1), font="Verdana 10 bold", bg="white")
-p2 = tk.Button(form, text=")", command=lambda: writer(p2), font="Verdana 10 bold", bg="white")
-
-en = tk.Button(form, text="=", command=calc_func, font="Verdana 10 bold", bg="white")
+en = tk.Button(form, text="=", command=calc_func,
+               font="Verdana 10 bold", bg="white")
 
 
-adop_but = tk.Button(form, text="Show Advanced Options", command=advanced_options_func, font="Verdana 10 bold", bg="white")
+adop_but = tk.Button(form, text="Show Advanced Options",
+                     command=advanced_options_func, font="Verdana 10 bold", bg="white")
 adop_but.place(relx=0.12, rely=0.76, relwidth=0.758, height=40)
 
 to.place(relx=0.12, rely=0.16, relwidth=0.16, relheight=0.10)
